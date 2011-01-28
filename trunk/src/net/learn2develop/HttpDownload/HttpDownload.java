22f41a3fb8543e9661bbf2fa9dac455573f3d5a6
daiwei89@gmail.com <daiwei89@gmail.com@d63646a1-5feb-cb13-158a-980d13331484>
2011-01-28 09:05:21 +0000

    Android app that can fetch source code from a webpage.

---
+++ trunk/src/net/learn2develop/HttpDownload/HttpDownload.java
@@ -0,0 +1,136 @@
+package net.learn2develop.HttpDownload;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLConnection;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element; 
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+import android.app.Activity;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Bundle;
+
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+
+public class HttpDownload extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+        
+        /*
+        Bitmap bitmap = 
+            DownloadImage(
+            "http://www.streetcar.org/mim/cable/images/cable-01.jpg");
+        ImageView img = (ImageView) findViewById(R.id.img);
+        img.setImageBitmap(bitmap);
+        */
+        
+        String str =
+            DownloadText("http://cs141hw2.appspot.com");
+        TextView txt = (TextView) findViewById(R.id.text);
+        txt.setText(str);     
+    }
+ 
+    
+    private Bitmap DownloadImage(String URL)
+    {        
+        Bitmap bitmap = null;
+        InputStream in = null;        
+        try {
+            in = OpenHttpConnection(URL);
+            bitmap = BitmapFactory.decodeStream(in);
+            in.close();
+        } catch (IOException e1) {
+            // TODO Auto-generated catch block
+            e1.printStackTrace();
+        }
+        return bitmap;                
+    }
+    
+    /* opens a connection to a HTTP server and returns an InputStream object */
+    private InputStream OpenHttpConnection(String urlString) throws IOException
+    {
+        InputStream in = null;
+        int response = -1;
+               
+        URL url = new URL(urlString); 
+        URLConnection conn = url.openConnection();
+                 
+        if (!(conn instanceof HttpURLConnection))                     
+            throw new IOException("Not an HTTP connection");
+        
+        try{
+            HttpURLConnection httpConn = (HttpURLConnection) conn;
+            httpConn.setAllowUserInteraction(false);
+            httpConn.setInstanceFollowRedirects(true);
+            httpConn.setRequestMethod("GET");
+            httpConn.connect(); 
+
+            response = httpConn.getResponseCode();                 
+            if (response == HttpURLConnection.HTTP_OK) {
+                in = httpConn.getInputStream();                                 
+            }                     
+        }
+        catch (Exception ex)
+        {
+            throw new IOException("Error connecting");            
+        }
+        return in;     
+    }
+    
+    
+    
+    private String DownloadText(String URL)
+    {
+        int BUFFER_SIZE = 2000;
+        InputStream in = null;
+        try {
+            in = OpenHttpConnection(URL);
+        } catch (IOException e1) {
+            // TODO Auto-generated catch block
+            e1.printStackTrace();
+            return "";
+        }
+        
+        InputStreamReader isr = new InputStreamReader(in);
+        int charRead;
+          String str = "";
+          char[] inputBuffer = new char[BUFFER_SIZE];          
+        try {
+            while ((charRead = isr.read(inputBuffer))>0)
+            {                    
+                //---convert the chars to a String---
+                String readString = 
+                    String.copyValueOf(inputBuffer, 0, charRead);                    
+                str += readString;
+                inputBuffer = new char[BUFFER_SIZE];
+            }
+            in.close();
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+            return "";
+        }    
+        return str;        
+    }
+}
